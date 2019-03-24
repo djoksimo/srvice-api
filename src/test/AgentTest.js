7176afe7a332296c75a877f3efe514f766614933
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-03-24 03:06:15 -0400

    added simple test for agent routes

---
+++ src/test/AgentTest.js
@@ -0,0 +1,58 @@
+process.env.NODE_ENV = 'test';
+
+const mongoose = require("mongoose");
+const Agent = require("../models/AgentModel");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require("../index");
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Agent', () => {
+  beforeEach((done) => {
+    Agent.remove({}, (err) => {
+      done();
+    });
+  });
+  describe('/GET/:id agent', () => {
+    it('it should GET an agent by the given id', (done) => {
+      let agent = new Agent({
+        email: "danilo@srvice.ca",
+        firstName: "Danilo",
+        lastName: "Joksimovic",
+        dateJoined: new Date(),
+        profilePictureUrl: "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg",
+        services: [],
+        location: "134 Columbia St. W",
+        languages: ["English", "French", "Serbian"],
+        company: "Srvice",
+        education: "University of Waterloo, Software Engineering",
+        certifications: ["Very Good at Coding (with honours)"],
+      });
+      agent.save((err, agent) => {
+        chai.request(server)
+          .get('/agent/' + agent.id)
+          .send(agent)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('email');
+            res.body.should.have.property('firstName');
+            res.body.should.have.property('lastName');
+            res.body.should.have.property('dateJoined');
+            res.body.should.have.property('profilePictureUrl');
+            res.body.should.have.property('services');
+            res.body.should.have.property('location');
+            res.body.should.have.property('languages');
+            res.body.should.have.property('company');
+            res.body.should.have.property('education');
+            res.body.should.have.property('certifications');
+            res.body.should.have.property('_id').eql(agent.id);
+            done();
+          });
+      });
+    });
+  });
+});
