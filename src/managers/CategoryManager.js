248f24b70bd68988786eef7b63e9752259d0a5dd
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-06-24 20:53:52 -0400

    [55] Update home categories (#32)

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -34,8 +34,16 @@ class CategoryManager {
   }
 
   async getHomeCategories() {
-    // TODO: Insert 8 initial categories:
-    const categoryIds = ["5c7cbbcae344b32a42dc06ad", "5c7cbc15e344b32a42dc06b2"];
+    const categoryIds = [
+      "5d116d75c88f3f36a7210acb",
+      "5d116ee94c533b38dab4e0ea",
+      "5d116f114c533b38dab4e0eb",
+      "5d116f224c533b38dab4e0ec",
+      "5d116f304c533b38dab4e0ed",
+      "5d116f3c4c533b38dab4e0ee",
+      "5d116f4f4c533b38dab4e0ef",
+      "5d116f5d4c533b38dab4e0f0",
+    ];
     try {
       const categoryDocumentsPromise = categoryIds.map(async id =>
         await this.categoryService.getCategoryByIdWithoutServices(id)
