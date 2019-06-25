77e365409e56768cfe4bf16ee95a2444a4032b07
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-06-24 20:42:30 -0400

    [54] Add iconUrl to category model (#31)

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -6,6 +6,7 @@ const { ObjectId } = Schema.Types;
 const categorySchema = Schema({
   _id: { type: ObjectId, auto: true, required: true },
   name: { type: "String", required: true },
+  iconUrl: { type: "String", required: true },
 }, { versionKey: false });
 
 const categoryModel = Mongoose.model("Category", categorySchema);
