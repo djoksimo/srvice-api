d1ddbc0ab9b5105ddd41fb48cf863cec815224ed
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-08-16 21:46:20 -0400

    [79] Fix and improve file upload (#55)
    * [79] Add path dependency
    
    [79] Fix file upload for production
    
    * [79] File upload working in App Engine
    
    Doesn't require writing to temp file anymore, aka faster and more secure :)))))

--- src/managers/FileManager.js
+++ src/managers/FileManager.js
@@ -1,12 +1,6 @@
-const { GoogleValues } = require("../values");
-
 class FileManager {
   constructor(FileService) {
     this.fileService = FileService;
-  }
-
-  getPublicUrl(fileName) {
-    return `https://storage.googleapis.com/${GoogleValues.BUCKET_NAME}/${fileName}.png`;
   }
 
   async uploadPictures(files) {
@@ -14,17 +8,10 @@ class FileManager {
       const resp = {
         publicPictureUrls: [],
       };
-      await Promise.all(files.map(async (file) => {
-        const { filename, path } = file;
-        await this.fileService.getSmallPngFromTemp(filename, path);
-        await this.fileService.uploadToBucket(`${path}.png`);
-        await this.fileService.removeTempFile(path);
-        await this.fileService.removeTempFile(`${path}.png`);
-        return resp.publicPictureUrls.push(this.getPublicUrl(filename));
-      }));
+      resp.publicPictureUrls = await Promise.all(files.map(async file => this.fileService.getPublicUrlFromUpload(file)));
       return { status: 200, json: resp };
     } catch (error) {
-      return { status: 500, json: error };
+      return { status: 500, json: error.toString() };
     }
   }
 }
