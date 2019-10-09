a39069088508b2ee377040fc91ec49394e30d7c6
fvuja12 <filip.vujadinovic@outlook.com>
2019-10-09 19:59:58 -0400

    added routes test and manager

--- src/test/index.js
+++ src/test/index.js
@@ -1,17 +1,23 @@
 const { ServiceManagerTest } = require("./managers");
 const { ServiceServiceTest } = require("./services");
+const { ServiceRoutesTest } = require("./http");
 
 process.env.NODE_ENV = "TEST";
 
 const tests = [
   new ServiceManagerTest(),
   new ServiceServiceTest(),
+  new ServiceRoutesTest(),
 ];
 
 
 tests.forEach(async (test) => {
   try {
-    await test.start();
+    if (process.env.NODE_ENV === "TEST") {
+      await test.start();
+    } else {
+      console.log("YOU TRIED TO USE THE WRONG DB, SHAME ON YOU");
+    }
   } catch (error) {
     console.log(error);
   }
