9f889ee7314687a8ac2d4a524d46df97ba647c92
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-10-09 22:37:21 -0400

    [82] Fixed linting and code style issues

--- src/test/http/ServiceRoutesTest.js
+++ src/test/http/ServiceRoutesTest.js
@@ -1,10 +1,10 @@
+const assert = require("assert");
+const chaiHttp = require("chai-http");
+const mongodb = require("mongodb");
+const chai = require("chai");
+const { describe } = require("mocha");
+
 const { ServiceModel } = require("../../models/");
-const assert = require('assert');
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const mongodb =require("mongodb");
-const server = require("../../index");
-const { ServiceManager } = require("../../bottle");
 
 const FAKE_JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhaW1AY3JlZHVwLnJ1IiwiaWF0IjoxNTcwMTUxMTEyfQ.CyYwspIRCudpljDo7QLawom6AKizB-lYo8cj9VxrfMw";
 
@@ -25,8 +25,8 @@ class ServiceRoutesTest {
     });
 
 
-    describe('/POST service', () => {
-      it('it should  POST a service', (done) => {
+    describe("/POST service", () => {
+      it("it should  POST a service", (done) => {
         const mockService = {
           agent: "5d969ab55e22efb586ab605f",
           category: "5d969ec1365ddec148ee5b0b",
@@ -35,7 +35,7 @@ class ServiceRoutesTest {
           pictureUrls: [
             "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
             "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg"
+            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
           ],
           phone: "4161234567",
           email: "haim@credup.ru",
@@ -48,25 +48,23 @@ class ServiceRoutesTest {
           radius: 10,
           averageServiceRating: 0,
           serviceRatings: [],
-          products: []
+          products: [],
         };
         const host = "http://localhost:5000";
         chai.request(host)
           .post('/service')
-          .set('content-type', 'application/json')
-          .set('token', FAKE_JWT_TOKEN)
-          .set('agentId', '5d969ab55e22efb586ab605f')
-          .set('email', 'haim@credup.ru')
+          .set("content-type", "application/json")
+          .set("token", FAKE_JWT_TOKEN)
+          .set("agentId", "5d969ab55e22efb586ab605f")
+          .set("email", "haim@credup.ru")
           .send(mockService)
           .end((err, res) => {     
-            assert.equal(201,res.status,"Fail: The status should be 201");
-            assert.ok(mongodb.ObjectID.isValid(res.body.serviceId),"Fail: MongoDB ID is not valid");
+            assert.strictEqual(201, res.status, "Fail: The status should be 201");
+            assert.ok(mongodb.ObjectID.isValid(res.body.serviceId), "Fail: MongoDB ID is not valid");
             done();
           });
-        });
+      });
     });
-
-
   }
 }
 
