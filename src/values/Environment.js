78ebc399aca99b953acade0b9351594cf6654b66
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-11-26 20:49:16 -0500

    [91] Fix invalid use of static in NODE10

--- src/values/Environment.js
+++ src/values/Environment.js
@@ -1,17 +1,9 @@
 const { env } = process;
 
-class Environment {
-  static PRODUCTION = "PRODUCTION";
-  static SANDBOX_01 = "SANDBOX_01";
-  static TEST = "TEST";
-
-  static getCurrentNodeEnv() {
-    return env.NODE_ENV;
-  }
-
-  static getGurrentPort() {
-    return env.PORT || "5000";
-  }
-}
-
-module.exports = Environment;
+module.exports = {
+  PRODUCTION: "PRODUCTION",
+  SANDBOX_01: "SANDBOX_01",
+  TEST: "TEST",
+  getCurrentNodeEnv: () => env.NODE_ENV,
+  getGurrentPort: () => env.PORT || "5000",
+};
