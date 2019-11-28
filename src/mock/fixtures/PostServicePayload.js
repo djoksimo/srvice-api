02b80008941f1e875a780094ddd244b54c8bb3d4
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-11-28 14:33:19 -0500

    [91] Fix payload vals

--- src/mock/fixtures/PostServicePayload.js
+++ src/mock/fixtures/PostServicePayload.js
@@ -1,5 +1,5 @@
 module.exports = {
-  agent: "5d969ab55e22efb586ab605f",
+  agent: "5cdf5367cbc99526685bd64f",
   category: "5d969ec1365ddec148ee5b0b",
   title: "GOOOD GOOD Service",
   description: "ASFAS ASF ASF ASF ASF ASF ASF ASF ASF ASFASFASFASFJASF ASFAJSJASFJASJF ASFASFJSAFAS ASF ASFAS F ASF ASFASFASF AFSAS",
@@ -9,7 +9,7 @@ module.exports = {
     "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
   ],
   phone: "4161234567",
-  email: "haim@credup.ru",
+  email: "mosss@gmail.com",
   inCall: true,
   outCall: true,
   remoteCall: true,
--- src/test/http/ServiceRoutesTest.js
+++ src/test/http/ServiceRoutesTest.js
@@ -27,6 +27,30 @@ class ServiceRoutesTest {
 
     describe("/POST service", () => {
       it("it should  POST a service", (done) => {
+        // TODO: temporarily put here as Filip's changes are in review
+        const mockService = {
+          agent: "5d969ab55e22efb586ab605f",
+          category: "5d969ec1365ddec148ee5b0b",
+          title: "GOOOD GOOD Service",
+          description: "ASFAS ASF ASF ASF ASF ASF ASF ASF ASF ASFASFASFASFJASF ASFAJSJASFJASJF ASFASFJSAFAS ASF ASFAS F ASF ASFASFASF AFSAS",
+          pictureUrls: [
+            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
+            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
+            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
+          ],
+          phone: "4161234567",
+          email: "haim@credup.ru",
+          inCall: true,
+          outCall: true,
+          remoteCall: true,
+          address: "3530 Atwater Ave, Montreal, QC H3H 1Y5",
+          latitude: 50.304922,  
+          longitude: -73.589814,
+          radius: 10,
+          averageServiceRating: 0,
+          serviceRatings: [],
+          products: [],
+        };
         
         const host = "http://localhost:5000";
         chai.request(host)
