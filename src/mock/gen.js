049e661ef8b3ff7e087cf98426b4da95a088bed3
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-11-28 14:17:18 -0500

    [91] Improve clarity by pivoting data to fixtures

--- src/mock/gen.js
+++ src/mock/gen.js
@@ -1,37 +1,14 @@
 const { MockServiceCreation } = require("./ServiceMock");
 const { Environment } = require("../values");
+const { PostServicePayload } = require("./fixtures");
 
 const mockServiceCreationClient = new MockServiceCreation();
 
 mockServiceCreationClient.start();
-
-const mockService = {
-  agent: "5cdf5367cbc99526685bd64f",
-  category: "5d116f5d4c533b38dab4e0f0",
-  title: "GOOOD GOOD Service",
-  description: "ASFAS ASF ASF ASF ASF ASF ASF ASF ASF ASFASFASFASFJASF ASFAJSJASFJASJF ASFASFJSAFAS ASF ASFAS F ASF ASFASFASF AFSAS",
-  pictureUrls: [
-    "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-    "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-    "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-  ],
-  phone: "4161234567",
-  email: "mosss@gmail.com",
-  inCall: true,
-  outCall: true,
-  remoteCall: true,
-  address: "3530 Atwater Ave, Montreal, QC H3H 1Y5",
-  latitude: 50.304922,  
-  longitude: -73.589814,
-  radius: 10,
-  averageServiceRating: 0,
-  serviceRatings: [],
-  products: [],
-};
 
 if (Environment.getCurrentNodeEnv() !== Environment.SANDBOX_01) {
   console.log("YOU ARE NOT USING THE SANDBOX DB");
   process.exit(1);
 }
 
-mockServiceCreationClient.generateFakeServices(50, mockService);
+mockServiceCreationClient.generateFakeServices(50, PostServicePayload);
