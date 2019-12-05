87b98f5860b6360d445061aab4cbe15e11dc06e5
fvuja12 <filip.vujadinovic@outlook.com>
2019-12-04 22:00:27 -0500

    84 Test Getting Services (#67)
    * added find service tests
    
    * [84] added get service by id test
    
    * added get nearby service tests
    
    * added get service tests for service manager
    
    * fixed linting errors
    
    * renamed healthy service and removed unused code
    
    * [84] added get service by id test
    
    * added get service tests for service manager
    
    * fixed linting errors
    
    * [84] Update lint config
    
    fix some linting issues
    
    * [84] fix lint script

--- src/test/http/ServiceRoutesTest.js
+++ src/test/http/ServiceRoutesTest.js
@@ -2,18 +2,24 @@ const assert = require("assert");
 const chaiHttp = require("chai-http");
 const mongodb = require("mongodb");
 const chai = require("chai");
+const server = require("../../index");
 const { describe } = require("mocha");
-
+const { HealthyService } = require("../fixtures/");
 const { ServiceModel } = require("../../models/");
+const { ServiceService } = require("../../bottle");
 
 const FAKE_JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhhaW1AY3JlZHVwLnJ1IiwiaWF0IjoxNTcwMTUxMTEyfQ.CyYwspIRCudpljDo7QLawom6AKizB-lYo8cj9VxrfMw";
 
 chai.use(chaiHttp);
 
 class ServiceRoutesTest {
-  
+  constructor() {
+    chai.request(server).get("/");
+  }
+
   async start() {
     this.testPostService();
+    this.testGetService();
   }
 
   testPostService() {
@@ -24,34 +30,8 @@ class ServiceRoutesTest {
       });
     });
 
-
     describe("/POST service", () => {
       it("it should  POST a service", (done) => {
-        // TODO: temporarily put here as Filip's changes are in review
-        const mockService = {
-          agent: "5d969ab55e22efb586ab605f",
-          category: "5d969ec1365ddec148ee5b0b",
-          title: "GOOOD GOOD Service",
-          description: "ASFAS ASF ASF ASF ASF ASF ASF ASF ASF ASFASFASFASFJASF ASFAJSJASFJASJF ASFASFJSAFAS ASF ASFAS F ASF ASFASFASF AFSAS",
-          pictureUrls: [
-            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-            "https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/baby_ja7a.svg",
-          ],
-          phone: "4161234567",
-          email: "haim@credup.ru",
-          inCall: true,
-          outCall: true,
-          remoteCall: true,
-          address: "3530 Atwater Ave, Montreal, QC H3H 1Y5",
-          latitude: 50.304922,  
-          longitude: -73.589814,
-          radius: 10,
-          averageServiceRating: 0,
-          serviceRatings: [],
-          products: [],
-        };
-        
         const host = "http://localhost:5000";
         chai.request(host)
           .post('/service')
@@ -59,7 +39,7 @@ class ServiceRoutesTest {
           .set("token", FAKE_JWT_TOKEN)
           .set("agentId", "5d969ab55e22efb586ab605f")
           .set("email", "haim@credup.ru")
-          .send(mockService)
+          .send(HealthyService)
           .end((err, res) => {     
             assert.strictEqual(201, res.status, "Fail: The status should be 201");
             assert.ok(mongodb.ObjectID.isValid(res.body.serviceId), "Fail: MongoDB ID is not valid");
@@ -68,6 +48,56 @@ class ServiceRoutesTest {
       });
     });
   }
+
+  testGetService() {
+    beforeEach((done) => {
+      ServiceModel.deleteMany({}, (err) => {
+        assert.ifError(err);
+        done();
+      });
+    });
+
+    describe('/GET service', () => {
+      it('it should GET a service by the given id', (done) => {
+        const mockService = new ServiceModel(HealthyService);
+        mockService.save((err, service) => {
+          chai.request(server)
+            .get('/service/' + service.id)
+            .send(service)
+            .end((err2, res) => {
+              Object.keys(HealthyService).forEach((x) => {
+                if (x in res.body) {
+                  assert.strictEqual(JSON.stringify(res.body[x]), JSON.stringify(HealthyService[x]), "Fail: Returned wrong value for " + x);
+                } else {
+                  assert.ok(false, "Fail: " + x + " should be in the body");
+                }
+              });
+              done();
+            });
+        });
+      });
+
+      it('it should GET all nearby services', (done) => {
+        const mockService = new ServiceModel(HealthyService);
+        const nearbyStr = "nearby?categoryId=" + mockService.category + "&lat=" + mockService.latitude + "&lng=" + mockService.longitude;
+        mockService.save((err, service) => {
+          chai.request(server)
+            .get('/service/' + nearbyStr)
+            .send(service)
+            .end((err2, res) => {
+              if (res.body.services.length !== 0) {
+                assert.strictEqual(res.body.services[0].agent._id.toString(), service.agent.toString(), "Fail: Incorrect agent id returned");
+                assert.strictEqual(res.body.services[0].category._id.toString(), service.category.toString(), "Fail: Incorrect category id returned");
+              } else {
+                assert.ok(false, "Fail: Should return a list of services - no service returned");
+              }
+              done();
+            });
+        });
+      });
+    });
+  }
+  
 }
 
 module.exports = ServiceRoutesTest;
