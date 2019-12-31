bbf3379a837b009971b8444d41c1bba536d79db1
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-12-31 04:42:09 -0500

    [101] fix and add more tests
    improve chai code usage

---
+++ src/enums/HTTPVerbs.js
@@ -0,0 +1,9 @@
+const HTTPVerbs = Object.freeze({
+  POST: "POST",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  GET: "GET",
+  DELETE: "DELETE",
+});
+
+module.exports = HTTPVerbs;
--- src/mock/fixtures/index.js
+++ src/mock/fixtures/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   PostServicePayload: require("./PostServicePayload"),
-  FakeJWTToken: require("./FakeJWTToken"),
+  MockAgentCredentials: require("./MockAgentCredentials"),
 };
