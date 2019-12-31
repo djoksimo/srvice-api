1953aabb8cac2763391b84e23e932a57cfaaafc7
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-12-31 02:31:17 -0500

    [101] move fixtures around

--- src/test/fixtures/index.js
+++ src/test/fixtures/index.js
@@ -1,3 +1,8 @@
+process.env.NODE_ENV = "TEST";
+process.env.NODE_ENV = "TEST";
+
 module.exports = {
   HealthyService: require("./HealthyService"),
+  HealthyOffering: require("./HealthyOffering"),
+  MockAgentCredentials: require("./MockAgentCredentials"),
 };
