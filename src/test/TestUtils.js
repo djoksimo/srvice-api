93908800265e9cfa289237a993e84ee6b6df5430
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2019-12-31 04:54:53 -0500

    [101] fix more tests

--- src/test/TestUtils.js
+++ src/test/TestUtils.js
@@ -3,7 +3,7 @@ const sinon = require("sinon");
 class TestUtils {
   // typeOfLog can be "log", "info", "warn" or "error")
   static disableLogs(typeOfLog) {
-    sinon.stub(console, typeOfLog); // disable console.log
+    sinon.stub(console, typeOfLog); // disable console.<typeOfLog>
   }
 }
 
