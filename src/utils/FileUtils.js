93b23086ddb9b98acc1c016ddd8dc8965d90233e
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2020-01-02 20:35:16 -0500

    [101] fix gen cli tool
    logic output error and typo

--- src/utils/FileUtils.js
+++ src/utils/FileUtils.js
@@ -2,9 +2,14 @@ const fs = require("fs");
 
 class FileUtils {
 
-  static writeToFile(fileName, content) {
-    fs.writeFile(fileName, content, (err) => {
-      if (err) throw err;
+  static async asyncWriteToFileOnce(fileName, content) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(fileName, content, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(null);
+      });
     });
   }
 }
