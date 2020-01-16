d90ac2b91c354478aead56bac4a36dfce8559c89
Danilo Joksimovic <danilo@srvice.ca>
2020-01-15 23:16:35 -0500

    [102] Add ability to confirm account without confirmation code (#78)
    * [102] pivot some values to src/values
    
    * [102] Add endpoint for confirming accounts without an account by email
    
    * [102] Add auth confirm account by adming test

--- src/test/http/index.js
+++ src/test/http/index.js
@@ -3,4 +3,5 @@ process.env.NODE_ENV = "TEST";
 module.exports = {
   ServiceRoutesTest: require("./ServiceRoutesTest"),
   OfferingRoutesTest: require("./OfferingRoutesTest"),
+  AuthenticationRoutesTest: require("./AuthenticationRoutesTest"),
 };
