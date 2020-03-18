8ee3128acb1d117e593fc49fc6ca6bb2a60f5e6a
Nipun Lamba <nipun00123@gmail.com>
2020-03-18 11:35:44 -0400

    added .js file

--- src/managers/CategoryManager.ts
+++ src/managers/CategoryManager.ts
@@ -1,5 +1,7 @@
 import { ObjectID } from "mongodb";
 
+import { CategoryValues } from "../values";
+
 import { Category } from "../types";
 import { CategoryModel } from "../models";
 
@@ -58,16 +60,7 @@ export default class CategoryManager {
   }
 
   async getHomeCategories() {
-    const categoryIds = [
-      "5d116d75c88f3f36a7210acb",
-      "5d116ee94c533b38dab4e0ea",
-      "5d116f114c533b38dab4e0eb",
-      "5d116f224c533b38dab4e0ec",
-      "5d116f304c533b38dab4e0ed",
-      "5d116f3c4c533b38dab4e0ee",
-      "5d116f4f4c533b38dab4e0ef",
-      "5d116f5d4c533b38dab4e0f0",
-    ];
+    const categoryIds = CategoryValues.categoryIds;
     try {
       const categoryDocumentsPromise = categoryIds.map(
         async (id) => await this.categoryService.getCategoryByIdWithoutServices(id),
