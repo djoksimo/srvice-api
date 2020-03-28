596ecf78cf2a8eef6f518c0b5d4a6d676b85d14d
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2020-03-28 03:41:12 -0400

    Add db name option

--- src/test/helpers/preTests.ts
+++ src/test/helpers/preTests.ts
@@ -2,5 +2,5 @@ import { ServerUtils } from "../../utilities/serverUtils";
 
 export const preTests = () => {
   const serverUtils = new ServerUtils();
-  serverUtils.startServerWithLocalDb();
+  serverUtils.startServerWithLocalDb(5002, "test-db");
 };
